2518192f0a001871ed1d0cb4cb09759a04cd8051
Upendra Rai <upendrarai02@gmail.com>
2024-04-04 14:29:06 +0530

    second day learning

--- upendra-codingott/src/App.js
+++ upendra-codingott/src/App.js
@@ -1,7 +1,23 @@
 function App(){
+
+  var x=10
+  var y=20
+  var z=x+y
+  console.log(z)
   return (
     <div>
-     <h1>Learnig React 1st Day</h1> 
+     <h1 style={{
+      color: 'Red',
+      fontSize: '80px',
+      backgroundColor: 'blue'
+     }}> Learnig React 1st Day</h1> 
+     <p style={{
+      color: 'blue',
+      fontSize: '40',
+      backgroundColor: 'pink'
+     }}>
+      This is Paragraphs {30+80}
+     </p>
     </div>
   )
 }
